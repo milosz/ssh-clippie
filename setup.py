49731e4948920a6ba577ca53c1943f9886464244
milosz <milosz@sleeplessbeastie.eu>
2024-02-02 09:15:23 +0100

    removed not needed include_package_data option

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name="ssh-clippie",
-    version="0.8.5",
+    version="0.8.6",
     author="Milosz",
     author_email="milosz@sleeplessbeastie.eu",
     url="https://github.com/milosz/ssh-clippie",
@@ -13,7 +13,6 @@ setup(
     install_requires=["click == 8.1.7", "python-magic == 0.4.27", "pyyaml == 6.0.1"],
     packages=["ssh_clippie", "ssh_clippie.utils"],
     package_data={"ssh_clippie":["permissions_definition.yaml"]},
-    include_package_data=True,
     entry_points={
         "console_scripts": [
             "ssh-clippie = ssh_clippie:cli",
