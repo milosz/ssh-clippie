3c422229bcad2b0910ab3640b70b51944f806932
milosz <milosz@sleeplessbeastie.eu>
2024-02-02 08:35:16 +0100

    added utils package and renamed main to __init__

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name="ssh-clippie",
-    version="0.8.3",
+    version="0.8.4",
     author="Milosz",
     author_email="milosz@sleeplessbeastie.eu",
     url="https://github.com/milosz/ssh-clippie",
@@ -11,7 +11,7 @@ setup(
     description="Small utility to check ssh client configuration (permissions and file types).",
     long_description=open('README.md').read(),
     install_requires=["click == 8.1.7", "python-magic == 0.4.27", "pyyaml == 6.0.1"],
-    packages=["ssh_clippie"],
+    packages=["ssh_clippie", "ssh_clippie.utils"],
     package_data={"ssh_clippie":["permissions_definition.yaml"]},
     include_package_data=True,
     entry_points={
