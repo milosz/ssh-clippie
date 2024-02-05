ed929ca183c52150b3bb780d89b355b323aca662
Milosz Galazka <milosz@sleeplessbeastie.eu>
2024-02-05 19:00:36 +0100

    add utils package in setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup
 
 setup(
     name="ssh-clippie",
-    version="0.8.1",
+    version="0.8.2",
     py_modules=["main"],
     install_requires=["click == 8.1.7", "python-magic == 0.4.27", "pyyaml == 6.0.1"],
+    packages=['utils'],
     entry_points={
         "console_scripts": [
             "ssh-clippie = main:cli",
